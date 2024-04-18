5ce603031cc728e70ac6b146981d0ec9aa7cd517
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-04-17 23:05:01 -0600

    Update Exercise using Generate Column

--- Assignment/04.17.2024/Exercise.sql
+++ Assignment/04.17.2024/Exercise.sql
@@ -27,13 +27,13 @@ WITH USER #2.
 
 -- Step 2
 -- 1. Create three tables
-# CREATE TABLE produc
+# CREATE TABLE product
 # (
 #     id   INT PRIMARY KEY AUTO_INCREMENT,
 #     name VARCHAR(255) NOT NULL,
 #     price DECIMAL(10, 2) DEFAULT 0.0
 # );
-
+#
 # CREATE TABLE brand
 # (
 #     id   INT PRIMARY KEY AUTO_INCREMENT,
@@ -53,19 +53,21 @@ WITH USER #2.
 #         ON UPDATE CASCADE
 # );
 
-# CREATE TABLE order_product_brand(
-#     id INT PRIMARY KEY AUTO_INCREMENT,
-#     product VARCHAR(255) NOT NULL,
-#     brand VARCHAR(255) NOT NULL,
-#     qty INT DEFAULT 0,
-#     price_per_unit DECIMAL(10, 2) DEFAULT 0.0
+# CREATE TABLE order_product_brand
+# (
+#     id             INT PRIMARY KEY AUTO_INCREMENT,
+#     product        VARCHAR(255) NOT NULL,
+#     brand          VARCHAR(255) NOT NULL,
+#     qty            INT            DEFAULT 0,
+#     price_per_unit DECIMAL(10, 2) DEFAULT 0.0,
+#     total_price    DECIMAL(10, 2) AS (CAST(qty AS DECIMAL(10, 2)) * price_per_unit) STORED
 # );
 
 # -- Step 3
 # -- 1. Insert five entities into each table
 
 # START TRANSACTION;
-
+#
 # INSERT INTO product (name, price)
 # VALUES ('Product 1', FLOOR(RAND() * 10) + 1),
 #        ('Product 2', FLOOR(RAND() * 10) + 1),
@@ -122,53 +124,138 @@ WITH USER #2.
 -- Step 4
 -- 1. Create three different queries using joins or subqueries to display the relationships between these tables with user #3.
 
--- Products and Brands
-# SELECT p.name AS product_name, b.name AS brand_name, pb.product_id, pb.brand_id, p.price * (1 + b.brand_markup) AS price
+-- Brands and products
+# SELECT b.name AS brand, p.name AS product, ROUND(p.price * (1 + b.brand_markup), 2) AS price_per_unit
 # FROM product p
-# JOIN product_brand pb ON p.id = pb.product_id
-# JOIN brand b ON b.id = pb.brand_id;
+#     JOIN product_brand pb ON p.id = pb.product_id
+#     JOIN brand b ON b.id = pb.brand_id;
 
 -- Orders
-# SELECT brand, product, qty, price_per_unit, ROUND(qty * price_per_unit, 2) AS total_price
+# SELECT *
 # FROM order_product_brand;
 
 -- Average price per product
-# SELECT p.name AS product_name, ROUND(AVG(p.price * (1 + b.brand_markup)),2) AS avg_price
-# FROM product p
-# JOIN product_brand pb ON p.id = pb.product_id
-# JOIN brand b ON b.id = pb.brand_id
+# SELECT p.name AS product, ROUND(AVG(p.price * (1 + b.brand_markup)), 2) AS avg_price_per_unit
+# FROM product_brand pb
+#     JOIN product p ON pb.product_id = p.id
+#     JOIN brand b ON pb.brand_id = b.id
 # GROUP BY 1;
 
--- Top brands sold
-# SELECT brand, COUNT(*) AS total_products_sold
+-- Expensive brands
+# SELECT b.name AS brand, ROUND(SUM(p.price * (1 + b.brand_markup)), 2) AS avg_price_per_unit
+# FROM product_brand pb
+#     JOIN product p ON pb.product_id = p.id
+#     JOIN brand b ON pb.brand_id = b.id
+# GROUP BY 1
+# ORDER BY avg_price_per_unit DESC;
+
+-- Expensive brands with total price
+# SELECT b.name AS brand, ROUND(SUM(p.price * (1 + b.brand_markup)), 2) AS total_price
+# FROM product_brand pb
+#     JOIN product p ON pb.product_id = p.id
+#     JOIN brand b ON pb.brand_id = b.id
+# GROUP BY 1
+# ORDER BY total_price DESC;
+
+-- Expensive Brands with products
+# SELECT b.name AS brand, GROUP_CONCAT(DISTINCT p.name SEPARATOR ', ') AS products, ROUND(p.price * (1 + b.brand_markup), 2) AS price_per_unit
+# FROM product_brand pb
+#     JOIN product p ON pb.product_id = p.id
+#     JOIN brand b ON pb.brand_id = b.id
+# GROUP BY 1, 3
+# ORDER BY price_per_unit DESC;
+
+-- Expensive Brands with products and total price
+# SELECT b.name AS brand, GROUP_CONCAT(DISTINCT p.name SEPARATOR ', ') AS products, ROUND(SUM(p.price * (1 + b.brand_markup)), 2) AS total_price
+# FROM product_brand pb
+#     JOIN product p ON pb.product_id = p.id
+#     JOIN brand b ON pb.brand_id = b.id
+# GROUP BY 1
+# ORDER BY total_price DESC;
+
+# -- Brands with most orders and total price
+# SELECT brand, COUNT(*) AS orders, ROUND(SUM(total_price), 2) AS total_price
 # FROM order_product_brand
 # GROUP BY 1
-# ORDER BY total_products_sold DESC;
-
-
--- Top products sold
-# SELECT product, COUNT(*) AS total_products_sold
+# ORDER BY total_price DESC;
+#
+# -- Products with most orders and total price
+# SELECT product, COUNT(*) AS orders, ROUND(SUM(total_price), 2) AS total_price
 # FROM order_product_brand
 # GROUP BY 1
-# ORDER BY total_products_sold DESC;
-
--- Top products sold by brand
-# SELECT brand, product, COUNT(*) AS total_products_sold
-# FROM order_product_brand
-# GROUP BY 1, 2
-# ORDER BY total_products_sold DESC;
-
--- Top products sold by brand with total price
-# SELECT brand, product, COUNT(*) AS total_products_sold, ROUND(SUM(qty * price_per_unit), 2) AS total_price
-# FROM order_product_brand
-# GROUP BY 1, 2
-# ORDER BY total_products_sold DESC;
-
--- Top brands sold with revenue
-# SELECT brand, COUNT(*) AS total_products_sold, ROUND(SUM(qty * price_per_unit), 2) AS total_revenue
+# ORDER BY total_price DESC;
+#
+# -- Best selling products by brand, orders, and total price
+# SELECT product,
+#        GROUP_CONCAT(DISTINCT brand SEPARATOR ', '),
+#        COUNT(*)                   AS orders,
+#        ROUND(SUM(total_price), 2) AS total_price
 # FROM order_product_brand
 # GROUP BY 1
-# ORDER BY total_products_sold DESC;
+# ORDER BY total_price DESC;
+#
+# -- Revenue by brand
+# SELECT brand, ROUND(SUM(total_price), 2) AS revenue
+# FROM order_product_brand
+# GROUP BY 1
+# ORDER BY revenue DESC;
+#
+# -- Brands with the most markup
+# SELECT b.name                                        AS brand,
+#        ROUND(AVG(p.price * (1 + b.brand_markup)), 2) AS avg_price_per_unit,
+#        ROUND(AVG(b.brand_markup), 2)                 AS markup
+# FROM product_brand pb
+#     JOIN product p ON pb.product_id = p.id
+#     JOIN brand b ON pb.brand_id = b.id
+# GROUP BY 1
+# ORDER BY markup DESC;
+#
+# -- Brands with the most markup and total price
+# SELECT b.name                                        AS brand,
+#        ROUND(SUM(p.price * (1 + b.brand_markup)), 2) AS total_price,
+#        ROUND(AVG(b.brand_markup), 2)                 AS markup
+# FROM product_brand pb
+#     JOIN product p ON pb.product_id = p.id
+#     JOIN brand b ON pb.brand_id = b.id
+# GROUP BY 1
+# ORDER BY markup DESC;
+#
+# -- Brands with the most markup and products
+# SELECT b.name                                        AS brand,
+#        GROUP_CONCAT(DISTINCT p.name SEPARATOR ', ')  AS products,
+#        ROUND(AVG(p.price * (1 + b.brand_markup)), 2) AS avg_price_per_unit,
+#        ROUND(AVG(b.brand_markup), 2)                 AS markup
+# FROM product_brand pb
+#     JOIN product p ON pb.product_id = p.id
+#     JOIN brand b ON pb.brand_id = b.id
+# GROUP BY 1
+# ORDER BY markup DESC;
+
+-- Brands with the most markup and products and total price
+SELECT b.name                                        AS brand,
+       GROUP_CONCAT(DISTINCT p.name SEPARATOR ', ')  AS products,
+       ROUND(SUM(p.price * (1 + b.brand_markup)), 2) AS total_price,
+       ROUND(AVG(p.price * (1 + b.brand_markup)), 2) AS avg_price_per_unit,
+       ROUND(AVG(b.brand_markup), 2)                 AS markup
+FROM product_brand pb
+    JOIN product p ON pb.product_id = p.id
+    JOIN brand b ON pb.brand_id = b.id
+GROUP BY 1
+ORDER BY markup DESC;
+
+-- Brands with the most markup and products and total price and orders
+SELECT b.name                                        AS brand,
+       GROUP_CONCAT(DISTINCT p.name SEPARATOR ', ')  AS products,
+       COUNT(*)                                      AS orders,
+       ROUND(SUM(p.price * (1 + b.brand_markup)), 2) AS total_price,
+       ROUND(AVG(p.price * (1 + b.brand_markup)), 2) AS avg_price_per_unit,
+       ROUND(AVG(b.brand_markup), 2)                 AS markup
+FROM product_brand pb
+    JOIN product p ON pb.product_id = p.id
+    JOIN brand b ON pb.brand_id = b.id
+GROUP BY 1
+ORDER BY markup DESC;
+
 
 -- Test Delete Cascade
 # START TRANSACTION;
